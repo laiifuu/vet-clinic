d0f4986b625ff00f85808cdcb8754d2432c9fa78
Hayet Fellah <hayet.fellah145@gmail.com>
2022-12-09 14:24:12 +0100

    Add vets, specializations and visits

--- schema.sql
+++ schema.sql
@@ -32,3 +32,27 @@ ALTER TABLE animals
     DROP COLUMN species,
     ADD COLUMN species_id int REFERENCES species(id),
     ADD COLUMN owner_id int REFERENCES owners(id);
+
+-- add tables
+CREATE TABLE vets (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar,
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    vet_id int REFERENCES vets(id),
+    species_id int REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+    vet_id int REFERENCES vets(id),
+    animal_id int REFERENCES animals(id),
+    visit_date date,
+    PRIMARY KEY (vet_id, animal_id, visit_date)
+);
+
+
+
